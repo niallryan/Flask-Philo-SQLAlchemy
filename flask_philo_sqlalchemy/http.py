cb916753219d502ed23012adffa74c5e72036e88
Niall Ryan <ryann4@tcd.ie>
2019-01-22 14:48:30 +0000

    fix config name

--- flask_philo_sqlalchemy/http.py
+++ flask_philo_sqlalchemy/http.py
@@ -9,7 +9,7 @@ class SQLAlchemyView(BaseResourceView):
         self.app = current_app._get_current_object()
         if 'FLASK_PHILO_SQLALCHEMY' not in self.app.config:
             raise ConfigurationError(
-                'Not configuration found for Flask-Philo-SQLAlchemy')
+                'Not configuration found for Flask_Philo_SQLAlchemy')
         ctx = _app_ctx_stack.top
         if ctx is not None:
             if not hasattr(ctx, 'sqlalchemy_pool'):
